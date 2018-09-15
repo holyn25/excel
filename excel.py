812f0d9fe2afd0936a326ef2d08765a6052a48d0
lyn <1192941918@qq.com>
2018-09-15 10:23:18 +0800

    0914

--- excel.py
+++ excel.py
@@ -1,8 +1,19 @@
 import xlwings as xl
 
-work_book = xl.Book(r'test.xlsx')
-data_range = work_book.sheets('sheet1').range('b1')
-data_range.value = [1, 2, 3, 1,1,1,1,1,1]
-work_book.save()
+work_book = xl.Book('fish.xlsx')
+data_range = work_book.sheets('fish1').range('b3:c228')
+data = data_range.value
+
+with open('data.actorId', 'w') as dst:
+    dst.writelines('<actor>')
+    dst.writelines('\n')
+    for id_str in data:
+        actor_id = str(int(id_str[0]))
+        actor_str = id_str[1]
+        format_str = '<actor id = "{}" str = "{}"/>'.format(actor_id, actor_str)
+        dst.write('\t')
+        dst.write(format_str)
+        dst.write('\n')
+    dst.writelines('</actor>')
 
 
